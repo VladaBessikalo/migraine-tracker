ddce130d04ea51d05a8858e047f0e2f4984e6c47
VladaBessikalo <bess.vlada@gmail.com>
2025-06-20 16:20:24 +0200

    style: responsive calendar

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,28 +1,31 @@
-import dayjs from 'dayjs';
-import updateLocale from 'dayjs/plugin/updateLocale'
+import dayjs from "dayjs";
+import updateLocale from "dayjs/plugin/updateLocale";
 
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-dayjs.extend(updateLocale)
-dayjs.updateLocale('en', {
-    weekStart: 1,
-})
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { Box } from "@mui/material";
+
+dayjs.extend(updateLocale);
+dayjs.updateLocale("en", {
+  weekStart: 1,
+});
 
 export default function Calendar() {
-
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en">
-      <DemoContainer
-        components={[
-          'StaticDatePicker',
-        ]}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        width="100%"
+        maxWidth="400px"
+        margin="0 auto"
+        p={2}
       >
-        <DemoItem>
-          <StaticDatePicker defaultValue={dayjs(Date())} />
-        </DemoItem>
-      </DemoContainer>
+        <StaticDatePicker defaultValue={dayjs(Date())} />
+      </Box>
     </LocalizationProvider>
   );
 }
