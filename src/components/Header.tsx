b3edf6378d8d78501865e6cc1a006b847ab47d38
VladaBessikalo <bess.vlada@gmail.com>
2025-07-30 18:32:08 +0200

    style: add bg colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   const { logout } = useAuth();
 
   return (
-    <AppBar>
+    <AppBar sx={{ backgroundColor: "#328e6e" }}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Migraine Tracker
