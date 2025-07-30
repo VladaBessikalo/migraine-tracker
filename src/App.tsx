50e3389d2dc3a463ce1cb1d1c5390a73a72c850b
VladaBessikalo <bess.vlada@gmail.com>
2025-07-30 17:59:13 +0200

    style: header possition

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,24 @@ function App() {
   useAuth();
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Header />
-      <Container>
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{
+          mt: 8,
+          flexGrow: 1,
+          pt: 5,
+          pb: 3,
+        }}
+      >
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
           <Route
